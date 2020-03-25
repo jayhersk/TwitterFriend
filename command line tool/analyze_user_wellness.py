aefc307606cd4cc19445ace0e4d7aeb8102b7a25
Jaylin Herskovitz <jayhersk@umich.edu>
2020-03-25 13:47:09 -0400

    wellness file structure

--- command line tool/analyze_user_wellness.py
+++ command line tool/analyze_user_wellness.py
@@ -1,1 +1,17 @@
 # analyze the wellness score for one user
+
+import json
+
+def user_wellness(screen_name):
+    # should return a dictionary that gives a wellness score for each week
+    # and each month that the user was active.
+    pass
+
+if __name__ == '__main__':
+    # pass in the username of the account you want to download
+    data = user_wellness("jayhersk")
+
+    # write the json file
+    with open(screen_name + '_wellness.json', 'w') as outfile:
+        json.dump(data, outfile, indent=2)
+
